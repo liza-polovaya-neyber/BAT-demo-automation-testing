e6c174e489647ec1ea1349fb55db9164ae054edd
dmitriy-grek-neyber <dmitriy.grek@neyber.co.uk>
2018-03-19 13:35:17 +0200

    AUTO-5: EmailTypes,StringHelper added

---
+++ BATDemoFramework/EmailService/EmailTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace BATDemoFramework.EmailService
+{
+    public class EmailTypes
+    {
+        public const string ConfirmYourEmail = "Confirm your email";
+
+    }
+}
---
+++ BATDemoFramework/Helpers/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BATDemoFramework.Helpers
+{
+    public static class StringHelper
+    {
+        public static IEnumerable<string> GetURLsWithMatchingPattern(string rawHtml, string matchingPattern)
+        {
+            Regex urlRx = new Regex(@"((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*", RegexOptions.IgnoreCase);
+
+            MatchCollection matches = urlRx.Matches(rawHtml);
+            foreach (Match match in matches)
+            {
+                if (match.Value.ToLower().Contains(matchingPattern.ToLower()))
+                {
+                    yield return match.Value;
+                }
+            }
+
+        }
+    }
+}
